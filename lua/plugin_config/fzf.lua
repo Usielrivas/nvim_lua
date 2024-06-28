36b41ce24acb83079167c6bc4f43abf100761507
Usiel Rivas <usielrivas00@gmail.com>
2024-06-28 10:35:50 -0600

    feat: fzf split horizontal

---
+++ lua/plugin_config/fzf.lua
@@ -0,0 +1,8 @@
+require'fzf-lua'.setup {
+  winopts = {
+    preview = {
+      layout = 'horizontal',  -- Layout horizontal para la vista previa
+      horizontal = 'up:40%',  -- La vista previa ocupará el 40% de la parte superior
+    },
+  },
+}
