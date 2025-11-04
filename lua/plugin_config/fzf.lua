b22ac0afcd25a8699e326b1919e560f181a0559e
Usielrivas <77363252+Usielrivas@users.noreply.github.com>
2025-11-04 08:39:59 -0600

    Increase preview height to 70% in fzf configuration

--- lua/plugin_config/fzf.lua
+++ lua/plugin_config/fzf.lua
@@ -2,7 +2,7 @@ require'fzf-lua'.setup {
   winopts = {
     preview = {
       layout = 'horizontal',  -- Layout horizontal para la vista previa
-      horizontal = 'up:40%',  -- La vista previa ocupará el 40% de la parte superior
+      horizontal = 'up:70%',  -- La vista previa ocupará el 40% de la parte superior
     },
   },
 }
