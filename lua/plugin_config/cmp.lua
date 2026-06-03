f03364af185c26db37bc4dd396497e63a01b31a1
Usielrivas <77363252+Usielrivas@users.noreply.github.com>
2026-06-03 10:15:36 -0600

    Refactor cmp configuration with new setup structure

--- lua/plugin_config/cmp.lua
+++ lua/plugin_config/cmp.lua
@@ -1,50 +1,51 @@
-local cmp = require'cmp'
-
-cmp.setup({
-  -- Configuración de snippets.
-  snippet = {
-    -- REQUERIDO - debes especificar un motor de snippets.
-    expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- Para usuarios de `vsnip`.
-      require('luasnip').lsp_expand(args.body) -- Para usuarios de `luasnip`.
-      -- require('snippy').expand_snippet(args.body) -- Para usuarios de `snippy`.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- Para usuarios de `ultisnips`.
-    end,
+require("blink.cmp").setup({
+  snippets = {
+    preset = "luasnip",
   },
-  -- Configuración de la ventana de autocompletado.
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  -- Mapeo de teclas.
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Acepta el elemento seleccionado actualmente. Establece `select` a `false` para confirmar solo los elementos seleccionados explícitamente.
 
-    ['<Tab>'] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-           cmp.select_next_item()
-         else
-           fallback()
-         end
-       end, { 'i', 's' }),
- 
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
- }),
-  -- Configuración de las fuentes de autocompletado.
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- Para usuarios de luasnip.
-  }, {
-    { name = 'buffer' },
-  })
+  keymap = {
+    preset = "default",
+
+    ["<CR>"] = { "accept", "fallback" },
+
+    ["<Tab>"] = {
+      "snippet_forward",
+      "select_next",
+      "fallback",
+    },
+
+    ["<S-Tab>"] = {
+      "snippet_backward",
+      "select_prev",
+      "fallback",
+    },
+  },
+
+  appearance = {
+    nerd_font_variant = "mono",
+  },
+
+  completion = {
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 200,
+    },
+
+    ghost_text = {
+      enabled = true,
+    },
+  },
+
+  signature = {
+    enabled = true,
+  },
+
+  sources = {
+    default = {
+      "lsp",
+      "snippets",
+      "buffer",
+      "path",
+    },
+  },
 })
