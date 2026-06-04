33eb63b4e7aa8108853310b63d878bc0221128dd
Usielrivas <77363252+Usielrivas@users.noreply.github.com>
2026-06-04 13:11:03 -0600

    Refactor cmp configuration by removing unused settings
    Removed appearance, completion, and signature settings from cmp configuration.

--- lua/plugin_config/cmp.lua
+++ lua/plugin_config/cmp.lua
@@ -21,25 +21,6 @@ require("blink.cmp").setup({
     },
   },
 
-  appearance = {
-    nerd_font_variant = "mono",
-  },
-
-  completion = {
-    documentation = {
-      auto_show = true,
-      auto_show_delay_ms = 200,
-    },
-
-    ghost_text = {
-      enabled = true,
-    },
-  },
-
-  signature = {
-    enabled = true,
-  },
-
   sources = {
     default = {
       "lsp",
